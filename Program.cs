8951d9a142fb698adf4a3240ce0556455627753a
Roberto Bastos <roberto.bastos@prolins.com.br>
2022-12-03 18:41:59 -0300

    implementa propriedades e metodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia a57 = new Nokia(numero: "1213727", modelo: "a57", imei: "654654654", memoria: 256);
+Iphone promax12 = new Iphone(numero: "1213727", modelo: "12 promax", imei: "654654654", memoria: 256);
+
+a57.Ligar();
+a57.ReceberLigacao();
+
+a57.InstalarAplicativo(nomeApp: "Instagram");
+
+promax12.Ligar();
+promax12.ReceberLigacao();
+
+promax12.InstalarAplicativo(nomeApp: "Instagram");
